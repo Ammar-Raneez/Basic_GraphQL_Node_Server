fb1cb88dce572b45283d39c0eccaafa6464d2c64
ammar <ammarraneez@gmail.com>
2022-01-31 21:36:03 +0530

    feat: link types together

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -15,16 +15,18 @@ const usersData = [
   { id: '5', name: 'Georgina', age: 36, profession: 'Painter' }
 ];
 const hobbyData = [
-  { id: '1', title: 'Programming', description: 'Some test description' },
-  { id: '2', title: 'Rowing', description: 'Some test description' },
-  { id: '3', title: 'Swimming', description: 'Some test description' },
-  { id: '4', title: 'Fencing', description: 'Some test description' },
-  { id: '5', title: 'Hiking', description: 'Some test description' }
+  { id: '1', title: 'Programming', description: 'Some test description', userId: '1' },
+  { id: '2', title: 'Rowing', description: 'Some test description', userId: '1' },
+  { id: '3', title: 'Swimming', description: 'Some test description', userId: '2' },
+  { id: '4', title: 'Fencing', description: 'Some test description', userId: '3' },
+  { id: '5', title: 'Hiking', description: 'Some test description', userId: '4' }
 ];
 const postData =  [
-  { id: '1', comment: 'Some test comment' },
-  { id: '2', comment: 'Some test comment' },
-  { id: '3', comment: 'Some test comment' },
+  { id: '1', comment: 'Some test comment', userId: '1' },
+  { id: '2', comment: 'Some test comment', userId: '1' },
+  { id: '3', comment: 'Some test comment', userId: '2' },
+  { id: '4', comment: 'Some test comment', userId: '3' },
+  { id: '5', comment: 'Some test comment', userId: '4' },
 ]
 
 // specify each entity
@@ -59,6 +61,12 @@ const HobbyType = new GraphQLObjectType({
     },
     description: {
       type: GraphQLString
+    },
+    user: {
+      type: UserType,
+      resolve(parent, args) {
+        return usersData.find((user) => user.id === parent.userId);
+      }
     }
   }
 });
@@ -72,6 +80,14 @@ const PostType = new GraphQLObjectType({
     },
     comment: {
       type: GraphQLString
+    },
+    // link two entities together
+    user: {
+      type: UserType,
+      resolve(parent, args) {
+        // in this case parent is post and child is user
+        return usersData.find((user) => user.id === parent.userId);
+      }
     }
   }
 });
